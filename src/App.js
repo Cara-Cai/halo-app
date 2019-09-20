79708af02cd3f2a7d5af7fe9a01941606a27c9da
蔡宇阳 <caiyuyang@caiyuyangdeMacBook-Air.local>
2019-09-20 16:19:11 +0800

    My first app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <body>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Welcome to My First Cool App!!!</h2>
+     <img width="500" 
+      alt="Freaking out"
+      src="https://i1.wp.com/herewomentalk.com/wp-content/uploads/2011/12/frustrated-writer-1.jpg"></img>
+     <p>This is how I'm try to code now.</p>
+     <h3>BUT</h3>
+     <img width="500" 
+      alt="successful"
+      src="https://cdn-blogs.tribune.com.pk/2010/09/TomJerry-640x480.jpg"></img>
+     <p>It can be easy like this some day.</p>
+     <h3>Wish me GOOD LUCK!</h3>
     </div>
+    </body>
+  
   );
+  
 }
 
 export default App;
